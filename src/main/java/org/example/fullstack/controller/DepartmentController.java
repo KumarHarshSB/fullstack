16985a5b8268586e68bdca4040553241052eb9ee
kumarharsh <kumar.harsh@wisetechglobal.com>
2025-02-02 18:00:51 +0530

    CORS Added

--- src/main/java/org/example/fullstack/controller/DepartmentController.java
+++ src/main/java/org/example/fullstack/controller/DepartmentController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/department")
+@CrossOrigin(origins = "http://localhost:4200")
 public class DepartmentController {
     private final DepartmentService departmentService;
 
--- src/main/java/org/example/fullstack/controller/EmployeeController.java
+++ src/main/java/org/example/fullstack/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/employee")
+@CrossOrigin(origins = "http://localhost:4200")
 public class EmployeeController {
     public EmployeeService employeeService;
 
